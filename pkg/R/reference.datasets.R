f3e6dbc58eac3b6ee526aa61801267f30dec54fc
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-17 11:00:55 +0000

    less defaults; allow arbitrary "U[a,b]" etc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hdi@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reference.datasets.R
+++ pkg/R/reference.datasets.R
@@ -1,125 +1,116 @@
-generate.reference.dataset <- function(n = 100,
-                                       p = 500,
-                                       s0 = 3,
-                                       xtype = c("toeplitz","exp.decay","equi.corr"),
-                                       btype = c("U[-2,2]","U[0,2]","U[0,4]","bfix1","bfix2","bfix10"),
-                                       permuted = FALSE,
-                                       iteration = 1)
+generate.reference.dataset <-
+    function(n, p, s0,
+             xtype = c("toeplitz", "exp.decay", "equi.corr"),
+             btype = "U[-2,2]", permuted = FALSE,
+             iteration = 1, do2S = TRUE)
 {
-  ## Purpose: generate the reference datasets used in the paper accompanying 
+  ## Purpose: generate the reference datasets used in the paper accompanying
   ##          this R package
   ## ----------------------------------------------------------------------
   ## Arguments:
   ## ----------------------------------------------------------------------
-  ## Author: Ruben Dezeure, Date: 6 Oct 2015, 16:27
+  ## Author: Ruben Dezeure, Date: 6 Oct 2015; 'do2S' and tweaks: Martin Maechler
 
-  ##Checking arguments
+  ## Checking arguments
   xtype <- match.arg(xtype)
-  btype <- match.arg(btype)
-  stopifnot(iteration > 0  && iteration <= 50)##only 50 versions of each were created
-  stopifnot(s0 >= 0)
-  
-  seed <- iteration + 2
-  set.seed(seed)
-  x <- generate.reference.x(n = n,
-                            p = p,
-                            xtype = xtype,
-                            permuted = permuted)
-  
-  set.seed(seed)
-  beta <- generate.reference.beta(p = p,
-                                  s0 = s0,
-                                  btype = btype)
-  out <- list(x=x,beta=beta)
-  return(out)
+  ## rather let generate.reference.beta() check
+
+  stopifnot(is.character(btype), length(btype) == 1,
+            n == as.integer(n), length(n) == 1, n >= 1,
+            p == as.integer(p), length(p) == 1, p >= 1,
+            s0== as.integer(s0),length(s0)== 1, 0 <= s0, s0 <= p)
+  do.seed <- !is.na(iteration)
+  if(do.seed)
+      stopifnot(iteration > 0  && iteration <= 50) ## only 50 versions of each were created
+  if(do.seed) {
+      seed <- iteration + 2
+      set.seed(seed)
+  }
+  x <- generate.reference.x(n = n, p = p, xtype = xtype,
+                            permuted = permuted, do2S = do2S)
+  if(do.seed) set.seed(seed)
+  beta <- generate.reference.beta(p = p, s0 = s0, btype = btype)
+  list(x=x, beta=beta)
 }
 
-generate.reference.x <- function(n,
-                                 p,
-                                 xtype,
-                                 permuted)
+generate.reference.x <- function(n, p, xtype, permuted, do2S = TRUE)
 {
-  ## Purpose: generate the reference design matrix used in the paper accompanying 
+  ## Purpose: generate the reference design matrix used in the paper accompanying
   ##          this R package
   ## ----------------------------------------------------------------------
   ## Arguments:
   ## ----------------------------------------------------------------------
   ## Author: Ruben Dezeure, Date: 6 Oct 2015, 16:27
-  
+
   Sigma <- switch(tolower(xtype),##in case capitalization was used
-                  "toeplitz"={
+                  "toeplitz" = {
                     indices <- toeplitz(0:(p-1))
                     cov <- (0.9)^abs(indices)
-                    solve(solve(cov))##This looks really stupid but the tiny numerical differences make it identical to the datasets used in the paper
+                    if(do2S) ## This looks really stupid but the tiny numerical differences
+                             ## make it identical to the datasets used in the paper
+                    solve(solve(cov))
                   },
-                  "equi.corr"={
+                  "equi.corr" = {
                     cov <- matrix(rep(0.8,p*p),p)
                     diag(cov) <- 1
-                    solve(solve(cov))##This looks really stupid but the tiny numerical differences make it identical to the datasets used in the paper
+                    if(do2S) ## see remark above
+                      solve(solve(cov))
                   },
-                  "exp.decay"={
+                  "exp.decay" = {
                     indices <- toeplitz(0:(p-1))
                     icov <- 0.4^(abs(indices)/5)
                     solve(icov)
                   },
-                  {
-                    stop("Invalid xtype: Please provide any of the following xtypes 'toeplitz', 'equi.corr' or 'exp.decay'")
-                  })
+                    stop("Invalid xtype: Please provide any of the following xtypes 'toeplitz', 'equi.corr' or 'exp.decay'"))
 
-  x <- mvrnorm(n,rep(0,p),Sigma)
+  x <- mvrnorm(n, rep(0,p), Sigma)
 
   if(permuted)
-    {
-      colindexing <- 1:ncol(x)
-      x <- x[,sample(colindexing)]
-    }
-
-  return(x)
+    x[, sample.int(ncol(x))]
+  else
+    x
 }
 
-generate.reference.beta <- function(p,
-                                    s0,
-                                    btype)
+generate.reference.beta <- function(p, s0, btype)
 {
-  ## Purpose: generate the reference coefficient vector used in the paper accompanying 
+  ## Purpose: generate the reference coefficient vector used in the paper accompanying
   ##          this R package
   ## ----------------------------------------------------------------------
   ## Arguments:
   ## ----------------------------------------------------------------------
-  ## Author: Ruben Dezeure, Date: 6 Oct 2015, 16:27
+  ## Author: Ruben Dezeure, Date: 6 Oct 2015;  tweaks by Martin Maechler
 
-  invalid.btype.txt <- "Invalid btype: Please provide a btype of the form 'bfix*' for a fixed value or 'U[*,*]', where * is a number."
-  
-  if(grepl("U\\[",btype) &&
-     grepl("\\]",btype) &&
-     (sapply(regmatches(btype, gregexpr(",", btype)), length)==1))##there should be one and only one comma in btype
-    {
-      ##generate random coefficients from a uniform distribution
-      split.btype <- strsplit(btype,",")[[1]]
+  stopifnot(s0 <= p, p >= 0, length(s0) == 1, length(p) == 1,
+            is.character(btype), length(btype) == 1)
+  invalid.btype.txt <- "Invalid btype: Please provide a btype of the form 'bfix*' for a fixed value or 'U[*,*]', where * are two numbers, the lower and upper bounds."
 
-      lower <- suppressWarnings(as.numeric(substr(split.btype[1],
-                                                  nchar("U[")+1,
-                                                  nchar(split.btype[1]))))##extract the lower bound
-      upper <- suppressWarnings(as.numeric(substr(split.btype[2],
-                                                  0,
-                                                  nchar(split.btype[2])-1)))##extract the upper bound
-      if(is.na(lower) || is.na(upper))##if there were any errors in the extraction
-        stop(invalid.btype.txt)
-      
-      beta <- c(runif(s0,lower,upper),rep(0,p-s0))      
-    }else{
-      if(grepl("bfix",btype)){
-        
-        b <- suppressWarnings(as.numeric(substr(btype,nchar("bfix")+1,nchar(btype))))##extract the bfix number
+  if(grepl("^U\\[",btype) &&
+     grepl("\\]$", btype) &&
+     sapply(regmatches(btype, gregexpr(",", btype)), length) == 1) {
+    ## there should be one and only one comma in btype
+    ## generate random coefficients from a uniform distribution
+    split.btype <- strsplit(sub("^U\\[", '', sub("\\]$", '', btype)),
+                            ",")[[1]]
 
-        if(is.na(b))##if there were any errors in the extraction
-          stop(invalid.btype.txt)
-        
-        beta <- c(rep(b,s0),rep(0,p-s0))
-        
-      }else{
-        stop(invalid.btype.txt)
-      }
-    }  
-  return(beta)
+    ## extract the lower and upper bounds:
+    lower <- as.numeric(split.btype[1])
+    upper <- as.numeric(split.btype[2])
+    if(is.na(lower) || is.na(upper) || length(lower) != 1 || length(upper) != 1)
+      ## if there were any errors in the extraction
+      stop(invalid.btype.txt)
+
+    b <- runif(s0,lower,upper)
+
+  } else if(grepl("^bfix", btype)) {
+
+    b <- as.numeric(sub("^bfix","", btype)) ## extract the bfix number
+    if(is.na(b))##if there were any errors in the extraction
+      stop(invalid.btype.txt)
+
+    b <- rep(b, s0)
+  }
+  else
+    stop(invalid.btype.txt)
+
+  c(b, rep(0, p-s0))
 }
--- pkg/man/generate.reference.dataset.rd
+++ pkg/man/generate.reference.dataset.rd
@@ -1,42 +1,70 @@
 \name{generate.reference.dataset}
-\alias{generate.reference.dataset}
-%- Also NEED an '\alias' for EACH other topic documented here.
 \title{Generate reference datasets}
-\description{This function generates a design matrix and coefficient vector corresponding to the reference linear model datasets of the hdi paper.} 
+\alias{generate.reference.dataset}
+\concept{reference datasets}
+\description{
+  This function generates a design matrix and coefficient vector
+  corresponding to the reference linear model datasets of the hdi paper.
+}
 \usage{
-generate.reference.dataset(n = 100, p = 500, s0 = 3,
-                           xtype, btype,
-			   permuted = FALSE,iteration = 1)
-}				     
+generate.reference.dataset(n, p, s0,
+                           xtype = c("toeplitz", "exp.decay", "equi.corr"),
+                           btype = "U[-2,2]",
+			   permuted = FALSE, iteration = 1, do2S = TRUE)
+}
 \arguments{
-  \item{n}{The sample size}
-  \item{p}{The number of coefficients in the linear model}
-  \item{s0}{The number of nonzero coefficients desired in the model}
-  \item{xtype}{The type of design matrix one wants to generate. This needs to be one of 'toeplitz','equi.corr' and 'exp.decay'}
-  \item{btype}{The type of coefficients one wants to generate. This needs to be one of 'U[-2,2]', 'U[0,2]', 'U[0,4]', 'bfix1', 'bfix2' and 'bfix10'.}
-  \item{permuted}{If the columns of the design matrix have to be permuted or not. (logical)}
-  \item{iteration}{Which of the 50 realizations of the design type and coefficients type one wants to generate.}
+  \item{n}{integer; the sample size \eqn{n} (paper had always \code{n = 100}).}
+  \item{p}{integer; the number of coefficients in the linear
+    model. (paper had always \code{p = 500}).}
+  \item{s0}{integer number of \emph{nonzero} coefficients desired in the
+    model; hence at most \code{p}.}
+  \item{xtype}{a \code{\link{character}} string specifying the type of design matrix
+    one wants to generate.  Must be one of \code{"toeplitz"},
+    \code{"equi.corr"} or \code{"exp.decay"}.}
+  \item{btype}{a \code{\link{character}} string specifying the type of
+    coefficients (\dQuote{beta}) one wants to generate.  In the hdi
+    paper, has been one of
+    "U[-2,2]", "U[0,2]", "U[0,4]", "bfix1", "bfix2" and "bfix10".  In
+    general, any string of the form \code{"U[a,b]"} or \code{"bfix<c>"}
+    is allowed, where \code{a}, \code{b}, and \code{<c>} must be numbers
+    (with \eqn{a \le b}{a <= b}).}
+  \item{permuted}{logical specifying if the columns of the design matrix
+    should be permuted.}
+  \item{iteration}{integer or \code{NA} specifying which of the 50
+    realizations of the design type and coefficients type one wants to
+    generate.  If \code{NA}, the current \code{\link{.Random.seed}} is
+    taken as usual in \R.}
+  \item{do2S}{logical indicating if in the case of \code{xtype}s
+    \code{"toeplitz"} or \code{"equi.corr"}, the \eqn{p \times p}{p * p}
+    covariance matrix should be inverted twice.  Must be true, to
+    regenerate the \eqn{X} matrices from the hdi paper exactly
+    \dQuote{to the last bit}.}
 }
 %\details{}
 \value{
-\item{x}{The generated design matrix}
-\item{beta}{The generated coefficient vector}
+  A \code{\link{list}} with components
+  \item{x}{the generated \eqn{n \times p}{n * p} design matrix \eqn{X}.}
+  \item{beta}{the generated coefficient vector \eqn{\beta} (\sQuote{beta}).}
 }
 %\references{
+%  hdi paper ?
 %}
-\author{Ruben Dezeure dezeure@stat.math.ethz.ch}
-%\note{}
-
+\author{Ruben Dezeure \email{dezeure@stat.math.ethz.ch}}
 \examples{
 ## Generate the first realization of the linear model with design matrix type Toeplitz and
 ## coefficients type uniform between -2 and 2
 
-dataset <- generate.reference.dataset(xtype = "toeplitz", btype = "U[-2,2]", iteration = 1)
-x <- dataset$x
-beta <- dataset$beta
+dset <- generate.reference.dataset(n = 80, p = 20, s0 = 3,
+                                   xtype = "toeplitz", btype = "U[-2,2]")
+x <- dset$x
+beta <- dset$beta
 
-##generate 100 response vectors of this linear model
-y <- x %*% beta + replicate(100,rnorm(nrow(x)))
+## generate 100 response vectors of this linear model
+y <- as.vector( x \%*\% beta ) + replicate(100, rnorm(nrow(x)))
+
+## Use  'beta_min' fulfilling  beta's  (non standard 'btype'):
+str(ds2 <- generate.reference.dataset(n = 50, p = 12, s0 = 3,
+                                      xtype = "exp.decay", btype = "U[0.1, 5]"))
 }
-\keyword{reference datasets}
-\keyword{linear model}
+\keyword{datagen}
+\keyword{regression}
